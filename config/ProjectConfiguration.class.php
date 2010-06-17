7801cdd81993b964f410c314a7e136cd39fd9cae
Micah Breedlove (druid628) <druid628@gmail.com>
2010-06-17 16:00:37 +0000

    Initial import

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once '/usr/local/symfony/1.4/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+    $this->enablePlugins('sfAdminThemejRollerPlugin');
+  }
+}
