f5ab89a18ce8a30c5e2ae07d05783a99d5b3ecb5
Micah Breedlove (druid628) <druid628@gmail.com>
2010-06-21 15:06:15 +0000

    edits to game edit page

--- lib/form/doctrine/GameForm.class.php
+++ lib/form/doctrine/GameForm.class.php
@@ -13,5 +13,12 @@ class GameForm extends BaseGameForm
   public function configure()
   {
 	  unset($this['created_at'], $this['updated_at']);
+
+	  if(!$this->isNew())
+	  {
+		  $ws = $this->getWidgetSchema();
+		  $ws['result'] = new sfWidgetFormChoice(array('label' => 'Result', 'choices' => array($this->getObject()->getTeam1() => $this->getObject()->getTeam1(), 'Draw' => 'Draw', $this->getObject()->getTeam2() => $this->getObject()->getTeam2())));
+	  }
+
   }
 }
