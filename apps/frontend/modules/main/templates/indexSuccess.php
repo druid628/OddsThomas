eb6a55fae84c0b3612f9f67c3611df6beb98d440
Micah Breedlove (druid628) <druid628@gmail.com>
2010-06-21 15:09:42 +0000

    made the welcome screen

--- apps/frontend/modules/main/templates/indexSuccess.php
+++ apps/frontend/modules/main/templates/indexSuccess.php
@@ -1,5 +1,7 @@
-<?PHP
+<BR/>
+<h3>Welcome to the betting pool!</h3>
 
-echo "HOME PAGE! now do something!";
-
-?>
+<P>Eventually, you will be able to see active games and your bets from this screen.<br/>
+Hopefully that will be tomorrow (22 June, 2010).</P>
+<br/>
+.::. DruiD .::.
