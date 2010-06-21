6e50568ed990906c30f680b1a092633386a432a2
Micah Breedlove (druid628) <druid628@gmail.com>
2010-06-21 20:57:05 +0000

    removed old data from main homepage

--- apps/frontend/modules/main/templates/indexSuccess.php
+++ apps/frontend/modules/main/templates/indexSuccess.php
@@ -22,7 +22,5 @@
 	</tbody>
 </table>
 
-<P>Eventually, you will be able to see active games and your bets from this screen.<br/>
-Hopefully that will be tomorrow (22 June, 2010).</P>
 <br/>
 .::. DruiD .::.
